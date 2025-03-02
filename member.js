10330451ce831f6ba17c4c7ddc8338df18057238
sdw2505 <sdw2505@outlook.com>
2025-03-02 19:44:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember()
+{
+    var skills = document.getElementById("skills");
+    if (skills.style.display === "block") {
+        skills.style.display = "none";
+    } else {
+        skills.style.display = "block";
+    }
+}
